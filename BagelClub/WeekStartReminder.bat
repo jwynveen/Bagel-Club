46cf9ca0d7c0557f1745569844a21c4340456621
jonwynveen <jwynveen@laughlin.com>
2012-01-23 10:07:44 -0600

    update curl path

--- BagelClub/WeekStartReminder.bat
+++ BagelClub/WeekStartReminder.bat
@@ -1,2 +1,2 @@
-curl.exe http://sites.laughlin.com/bagelclub/Home/SendWeekStartReminderEmail
+C:\Web\BagelClub\BagelClub\curl.exe http://sites.laughlin.com/bagelclub/Home/SendWeekStartReminderEmail
 ::pause
