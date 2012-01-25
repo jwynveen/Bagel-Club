99fd6587285b71843654b0861a92e78f873960b0
jonwynveen <jwynveen@laughlin.com>
2012-01-25 10:49:42 -0600

    fix home action routing

--- BagelClub/Global.asax.cs
+++ BagelClub/Global.asax.cs
@@ -28,7 +28,8 @@ namespace BagelClub
 			routes.MapRoute(
 				"Default", // Route name
 				"{controller}/{action}/{id}", // URL with parameters
-				new { controller = "Home", action = "Index" } // Parameter defaults
+				new { controller = "Home", action = "Index", id = UrlParameter.Optional }, // Parameter defaults
+				new { controller = "(Mail|MailPreview|Bageller)"}
 			);
 			routes.MapRoute(
 				"Home-Default", // Route name
